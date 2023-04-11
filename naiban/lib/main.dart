8f53f076cb5d0229fb1e1c39f05a0673c51581d6
PoyitoBv <56709212+PoyitoBv@users.noreply.github.com>
2023-04-11 15:31:47 -0600

    Edit Main

---
+++ naiban/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import '../providers/_providers.dart';
+import '../routes/routes.dart';
+import '../themes/my_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LogInOutProvider()),
+        ChangeNotifierProvider(create: (_) => RegisterProvider()),
+      ],
+      child: const App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Naiban',
+      theme: AppTheme.theme,
+      initialRoute: 'register',
+      routes: appRoutes,
+    );
+  }
+}
